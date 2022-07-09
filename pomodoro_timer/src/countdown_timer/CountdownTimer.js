2166f949182aaa12901c258c32f8644a301b7240
Remastered21 <mykim1891@hotmail.com>
2022-07-08 22:19:56 -0700

    hard-coded timer running

--- pomodoro_timer/src/countdown_timer/CountdownTimer.js
+++ pomodoro_timer/src/countdown_timer/CountdownTimer.js
@@ -1,42 +1,72 @@
 import React, { Component } from 'react'
 
 class CountdownTimer extends Component {
-    componentDidMount() {
+  state = {
+    original_time: 59000, // TODO: replace hard code: 5900 ms
+    paused_time: 0
+  }
 
-    }
+  componentDidMount() {
+    // ? Run When Component is created/inserted in DOM
+    this.interval = setInterval(() => this.tick(), 10); // tick every ms
+  }
 
-    render() {
-        return (
-            <div>
-                <h1>Name of the task here</h1>
+  componentWillUnmount() {
+    // ? Run function when component is destroyed/removed from DOM
+    clearInterval(this.interval) // clear interval
+  }
 
-                <div className='countdown-container'>
-                    <div className='countdown-element hours-c'>
-                        <p className='big-text'>0</p>
-                        <span>hours</span>
-                    </div>
+  tick() {
+    this.setState(state => ({ original_time: state.original_time -= 10 }));
+    console.log("current ms left:", this.state.original_time);
+  }
 
-                    <div className='countdown-element'>
-                        <p className='big-text'>:</p>
-                    </div>
+  formatTime(secs) {
+    let hours = Math.floor(secs / 3600 / 1000);
+    let minutes = Math.floor(secs / 60 / 1000);
+    let seconds = Math.floor(secs / 1000 % 60);
 
-                    <div className='countdown-element mins-c'>
-                        <p className='big-text'>0</p>
-                        <span>mins</span>
-                    </div>
+    return { hours, minutes, seconds }
+  }
 
-                    <div className='countdown-element'>
-                        <p className='big-text'>:</p>
-                    </div>
+  handlePause(e) {
+    this.setState(state => ({ paused_time: this.state.original_time }));
 
-                    <div className='countdown-element secs-c'>
-                        <p className='big-text'>0</p>
-                        <span>secs</span>
-                    </div>
-                </div>
-            </div >
-        )
-    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Name of the task here</h1>
+
+        <div className='countdown-container'>
+          <div className='countdown-element hours-c'>
+            <p className='big-text'>{this.formatTime(this.state.original_time).hours}</p>
+            <span>hours</span>
+          </div>
+
+          <div className='countdown-element'>
+            <p className='big-text'>:</p>
+          </div>
+
+          <div className='countdown-element mins-c'>
+            <p className='big-text'>{this.formatTime(this.state.original_time).minutes}</p>
+            <span>mins</span>
+          </div>
+
+          <div className='countdown-element'>
+            <p className='big-text'>:</p>
+          </div>
+
+          <div className='countdown-element secs-c'>
+            <p className='big-text'>{this.formatTime(this.state.original_time).seconds}</p>
+            <span>secs</span>
+          </div>
+        </div>
+        <button onClick={this.handlePause}>Pause!</button>
+      </div >
+    )
+  }
 }
 
 export default CountdownTimer;
--- pomodoro_timer/src/countdown_timer/script.js
+++ pomodoro_timer/src/countdown_timer/script.js
@@ -7,22 +7,22 @@ const secsEl = document.getElementById('secs');
 const newYears = "1 Jan 2023"
 
 function countdown() {
-    const newYearsDate = new Date(newYears)
-    const currentDate = new Date();
-    
-    const totalSeconds = (newYearsDate - currentDate) / 1000;
+  const newYearsDate = new Date(newYears)
+  const currentDate = new Date();
 
-    const hours = Math.floor(totalSeconds / 3600) % 24;
-    const minutes = Math.floor(totalSeconds / 60) % 24;
-    const seconds = Math.floor(totalSeconds) % 60;
+  const totalSeconds = (newYearsDate - currentDate) / 1000;
 
-    hoursEl.innerHTML = formatTime(hours);
-    minsEl.innerHTML = formatTime(minutes);
-    secsEl.innerHTML = formatTime(seconds);
+  const hours = Math.floor(totalSeconds / 3600) % 24;
+  const minutes = Math.floor(totalSeconds / 60) % 24;
+  const seconds = Math.floor(totalSeconds) % 60;
+
+  hoursEl.innerHTML = formatTime(hours);
+  minsEl.innerHTML = formatTime(minutes);
+  secsEl.innerHTML = formatTime(seconds);
 }
 
 function formatTime(time) {
-    return time < 10 ? (`0${time}`) : time;
+  return time < 10 ? (`0${time}`) : time;
 }
 
 // Initial call
